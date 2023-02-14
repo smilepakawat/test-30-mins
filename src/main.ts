5793401c43994ccb67b02147211479ef10d1a958
Smile14 <smile.pakawat@gmail.com>
2023-02-14 15:37:04 +0700

    add prefix path api

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  await app.setGlobalPrefix('apis')
+  await app.init()
   await app.listen(3000);
 }
 bootstrap();
